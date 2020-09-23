c688aabb2798479ca0431ab2de3ec6fa9f4c1a38
Lee Laptop <leegphillips@yahoo.co.uk>
2020-09-23 20:26:59 +0100

    Added MarketAggregator

---
+++ src/main/java/org/github/leegphillips/mongex/SortingFactory.java
@@ -0,0 +1,8 @@
+package org.github.leegphillips.mongex;
+
+import org.bson.Document;
+
+public class SortingFactory {
+    public final static Document EARLIEST = new Document("date", 1);
+    public final static Document LATEST = new Document("date", -1);
+}
