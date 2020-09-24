79f565fe09e8978a085901629608caa2ef607682
Lee Laptop <leegphillips@yahoo.co.uk>
2020-09-24 18:02:04 +0100

    Reimplemnted CandleLoader for foundation for variable duration candles

--- src/main/java/org/github/leegphillips/mongex/CandleFactory.java
+++ src/main/java/org/github/leegphillips/mongex/CandleFactory.java
@@ -4,22 +4,17 @@ import org.apache.commons.csv.CSVRecord;
 import org.bson.Document;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class CandleFactory {
-    private static final SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    // different to the other one
-    private static final SimpleDateFormat STRING_TO_DATE = new SimpleDateFormat("yyyyMMddHHmmssSSS");
-
-    public Document create(List<CSVRecord> records, String pair) throws ParseException {
+    public Document create(List<CSVRecord> records, String pair, String tickSize, LocalDateTime batchCeiling) {
         TickFacade first = new TickFacade(records.get(0));
         BigDecimal open = first.getMid();
         BigDecimal close = new TickFacade(records.get(records.size() - 1)).getMid();
-        long timestamp = first.getTimestamp();
         BigDecimal low = first.getMid();
         BigDecimal high = first.getMid();
         for (CSVRecord record : records.size() > 1 ? records.subList(1, records.size() - 1) : records) {
@@ -29,10 +24,9 @@ public class CandleFactory {
         }
 
         Document candle = new Document();
-        candle.append("timestamp", timestamp);
-        Date tickDate = STRING_TO_DATE.parse(Long.toString(first.getTimestamp()));
+        candle.append("duration", tickSize);
         candle.append("pair", pair);
-        candle.append("date", FORMATTER.format(tickDate));
+        candle.append("timestamp", FORMATTER.format(batchCeiling));
         candle.append("open", open);
         candle.append("high", high);
         candle.append("low", low);
