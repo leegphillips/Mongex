836c5ce51d15b88c7c60344535441da0d68147e0
Lee Laptop <leegphillips@yahoo.co.uk>
2020-09-26 21:16:49 +0100

    Refactor for Tick

--- src/main/java/org/github/leegphillips/mongex/Tick.java
+++ src/main/java/org/github/leegphillips/mongex/Tick.java
@@ -18,24 +18,21 @@ public class Tick {
     private final BigDecimal bid;
     private final BigDecimal ask;
     private final BigDecimal mid;
-    private boolean error = false;
+    private final boolean error;
 
-    private Tick(LocalDateTime timestamp, BigDecimal bid, BigDecimal ask, BigDecimal mid) {
+    private Tick(LocalDateTime timestamp, BigDecimal bid, BigDecimal ask) {
         this.timestamp = timestamp;
         this.bid = bid;
         this.ask = ask;
-        this.mid = mid;
+        this.mid = ask.add(bid).divide(BigDecimal.valueOf(2), 4, RoundingMode.HALF_EVEN);
+        this.error = ask.compareTo(BigDecimal.ZERO) < 0 || bid.compareTo(BigDecimal.ZERO) < 0;
     }
 
     public static Tick create(@NonNull CSVRecord record) {
         LocalDateTime timestamp = LocalDateTime.parse(record.get(0), STR2DATE);
         BigDecimal ask = new BigDecimal(record.get(1).trim());
         BigDecimal bid = new BigDecimal(record.get(2).trim());
-        Tick tick = new Tick(timestamp,
-                bid,
-                ask,
-                ask.add(bid)
-                        .divide(BigDecimal.valueOf(2), 4, RoundingMode.HALF_EVEN));
+        return new Tick(timestamp, bid, ask);
 
 // the data seems to have an issue whereby the bid and ask get swapped often
 // we don't care too much about this because we are interested in the generated mid only
@@ -44,10 +41,6 @@ public class Tick {
 //            LOG.warn(String.format("%s bid greater than ask %s %s", tick.timestamp, tick.bid, tick.ask));
 //            failed = true;
 //        }
-
-        tick.error = tick.ask.compareTo(BigDecimal.ZERO) < 0 || tick.bid.compareTo(BigDecimal.ZERO) < 0;
-
-        return tick;
     }
 
     public LocalDateTime getTimestamp() {
