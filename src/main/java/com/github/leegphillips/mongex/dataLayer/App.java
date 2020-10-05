e59370434777beacca9b6080a476d6187c66a586
Lee Laptop <leegphillips@yahoo.co.uk>
2020-10-05 22:53:43 +0100

    Removed initial candle create from main App

--- src/main/java/com/github/leegphillips/mongex/dataLayer/App.java
+++ src/main/java/com/github/leegphillips/mongex/dataLayer/App.java
@@ -4,7 +4,6 @@ import com.github.leegphillips.mongex.dataLayer.ma.MALoader;
 
 public class App {
     public static void main(String[] args) {
-        CandleLoader.main(args);
         FullCandleSeriesChecker.main(args);
         MALoader.main(args);
     }
